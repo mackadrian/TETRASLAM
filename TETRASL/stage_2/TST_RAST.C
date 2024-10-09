41750a0b871c37a15de61665b73e9c1acd2ef4b2
Mack Adrian Bautista <113152526+mackadrian@users.noreply.github.com>
2024-10-09 17:12:35 -0600

    Work on plotting fonts using bitmap_8

--- TETRASL/stage_2/TST_RAST.C
+++ TETRASL/stage_2/TST_RAST.C
@@ -1,7 +1,8 @@
 #include <osbind.h>
 #include "raster.h"
+#include "font.h"
 
-#define HEIGHT 62
+#define HEIGHT 61
 const UINT16 test[HEIGHT] = {
 	0xffff,
 	0xe007,
@@ -63,29 +64,39 @@ const UINT16 test[HEIGHT] = {
 	0xb81d,
 	0xf00f,
 	0xe007,
-	0xffff,
 	0xffff};
 
 int main()
 {
 	UINT16 *base_16 = (UINT16 *)Physbase();
+	UINT8 *base_8 = (UINT8 *)Physbase();
 
+	/* Set screen to white.*/
 	printf("\033E\033f\n");
 	Cnecin();
+	/* Test 1: plot at at left upper corner boundary */
+	plot_bitmap_16(base_16, 0, 0, test, HEIGHT);
+	/*Test 2: plot at the right upper corner boundary*/
+	plot_bitmap_16(base_16, 639, 0, test, HEIGHT);
+	/*Test 3: plot at the left bottom corner boundary*/
+	plot_bitmap_16(base_16, 0, 323, test, HEIGHT);
+	/*Test 4: plot at the right bottom corner boundary*/
+	plot_bitmap_16(base_16, 639, 323, test, HEIGHT);
+	Cnecin();
+	clear_screen(base_16);
 
-	/* Test 1: plot at 0,0. */
-	clear_screen();
+	Cnecin();
+	/*Test 5: overlapping pixels*/
 	plot_bitmap_16(base_16, 0, 0, test, HEIGHT);
 	Cnecin();
+	plot_bitmap_16(base_16, 17, 0, test, HEIGHT);
 	Cnecin();
-	plot_bitmap_16(base_16, 320, 200, test, HEIGHT);
-	/*
+	plot_bitmap_16(base_16, 32, 0, test, HEIGHT);
 	Cnecin();
-	plot_bitmap_16(base_16, 2, 0, test, HEIGHT);
-	Cnecin();
-	plot_bitmap_16(base_16, 3, 0, test, HEIGHT);
-	Cnecin();
-	plot_bitmap_16(base_16, 4, 0, test, HEIGHT);
-	*/
+	clear_screen(base_16);
+
+	/*Test 6: plotting fonts*/
+	plot_bitmap_8(base_8, 0, 0, font, 8);
+
 	return 0;
 }
