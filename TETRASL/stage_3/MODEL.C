0b57075d60867d88a367ec2a2719dcb4d783e54e
Mack Adrian Bautista <113152526+mackadrian@users.noreply.github.com>
2024-10-14 23:12:05 -0600

    stage 3 and stage 4

---
+++ TETRASL/stage_3/MODEL.C
@@ -0,0 +1,14 @@
+#include "model.h"
+
+bool check_collision(Tetromino *piece, Tower *tower, Field *field, int new_x, int new_y)
+{
+  
+}
+
+void move_piece(Player *piece){
+
+}
+
+
+
+
---
+++ TETRASL/stage_3/t_model.C
@@ -0,0 +1,12 @@
+#include "model.h"
+#include <osbind.h>
+
+int main ()
+{
+  /* Set screen to white.*/
+  printf("\033E\033f\n");
+
+
+
+  return 0;
+}
